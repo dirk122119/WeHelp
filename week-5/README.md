21eeddc3d4ad5502b66cbb7ae213efe79fe99d5e
dirk122119 <dirk122119@gmail.com>
2022-10-18 23:35:35 +0800

    finish week-5

--- week-5/README.md
+++ week-5/README.md
@@ -1,21 +1,26 @@
 # 要求三:SQL CRUD
 * 使用 INSERT 指令新增一筆資料到 member 資料表中，這筆資料的 username 和 password 欄位必須是 test。
+
 ![](/week-5/img/insertMember.png)
 檢查
 ![](/week-5/img/checkInsertMember.png)
 
 * 使用 SELECT 指令取得所有在 member 資料表中的會員資料。
+
 ![](/week-5/img/checkAllMember.png)
 
 * 使用 SELECT 指令取得所有在 member 資料表中的會員資料，並按照 time 欄位，由
 近到遠排序。
+
 ![](/week-5/img/sortAllMember.png)
 
 * 使用 SELECT 指令取得 member 資料表中第 2 ~ 4 共三筆資料，並按照 time 欄位，
 由近到遠排序。( 並非編號 2、3、4 的資料，而是排序後的第 2 ~ 4 筆資料 )。
+
 ![](/week-5/img/get2-4Member.png)
 
 * 使用 SELECT 指令取得欄位 username 是 test 的會員資料。
+
 ![](/week-5/img/getUsernameTest.png)
 
 * 使用 SELECT 指令取得欄位 username 是 test、且欄位 password 也是 test 的資料。
@@ -25,17 +30,33 @@
 * 使用 UPDATE 指令更新欄位 username 是 test 的會員資料，將資料中的 name 欄位改
 成 test2。
 
-`safe_update`
+safe_update
 ![](/week-5/img/safe_update.png)
-`改名`
+改名
 ![](/week-5/img/renametest.png)
 
 # 要求四:SQL Aggregate Functions
 * 取得 member 資料表中，總共有幾筆資料 ( 幾位會員 )。
+
 ![](/week-5/img/count%20rows.png)
 
 * 取得 member 資料表中，所有會員 follower_count 欄位的總和。
+
 ![](/week-5/img/checkMemberSumandAvg.png)
+
 * 取得 member 資料表中，所有會員 follower_count 欄位的平均數。
+
 ![](/week-5/img/checkMemberSumandAvg.png)
+
 # 要求五
+*使用 SELECT 搭配 JOIN 語法，取得所有留言，結果須包含留言者會員的姓名。
+
+![](/week-5/img/joinAll.png)
+
+*使用 SELECT 搭配 JOIN 語法，取得 member 資料表中欄位 username 是 test 的所有留言，資料中須包含留言者會員的姓名。
+
+![](/week-5/img/jointest.png)
+
+*使用 SELECT、SQL Aggregate Functions 搭配 JOIN 語法，取得 member 資料表中欄位 username 是 test 的所有留言平均按讚數。
+
+![](/week-5/img/joinavg.png)
---
+++ week-5/data.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `follower_count` int unsigned NOT NULL DEFAULT '0',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'test2','test','test',10,'2022-10-17 16:46:23'),(2,'no.2','test2','test2',5,'2022-10-17 16:47:50'),(3,'no.3','test3','test3',20,'2022-10-17 16:48:18'),(4,'no.4','test4','test4',50,'2022-10-17 16:48:28'),(5,'陳冠守','dirk122119','secret',500,'2022-10-17 16:48:57');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `like_count` int unsigned NOT NULL DEFAULT '0',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,1,'good',100,'2022-10-17 17:37:07'),(2,1,'nice',130,'2022-10-17 17:37:20'),(3,1,'have a nice today',330,'2022-10-17 17:37:37'),(4,2,'How are you?',334,'2022-10-17 17:38:02');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-18 23:32:16
